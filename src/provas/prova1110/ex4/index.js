5ea05eb29e9d3990044d360cb1a436cdbe9643a5
milena <milena.custodio.senna@gmail.com>
2022-10-11 11:09:38 -0300

    prova2

---
+++ src/provas/prova1110/ex4/index.js
@@ -0,0 +1,54 @@
+import React, { useState} from 'react'
+import './index.scss'
+
+
+
+
+export default function Calcularsalario() {
+
+    const [calcular, setCalcular] = useState([]);
+    const [salario, setsalario] = useState([]);
+    const [bonus, setbonus] = useState([]);
+    const [desc, setDesc] = useState([]);
+
+    function Salario(){
+        let qtd = salario * (bonus/100);
+        const total = qtd - desc;
+        setCalcular(total);
+
+    }
+
+
+
+
+    return (
+
+        <main> 
+        <section className='pagina-salario'>
+
+            <h1> Calcular Salário </h1> 
+
+            <div>
+                Salário: <input type='number' value={salario} onChange={e => setsalario(Number(e.target.value))}  /> 
+                Bônus: <input type='number' value={bonus} onChange={e => setbonus(Number(e.target.value))}/> 
+                Desconto: <input type='numer' value={desc} onChange={e => setDesc(Number(e.target.value))} /> 
+                
+                <button onClick={Salario}> Calcular </button>
+            </div>
+
+            <h1> Seu salário liquido é  de{calcular} </h1>
+
+
+
+         </section>
+
+
+        </main>
+
+
+     
+
+        
+        
+    )
+}
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,9 @@ import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import Exercicio1 from './provas/prova0710/ex1/index.js'
 import Exercicio2 from './provas/prova0710/ex2/index.js'
 import Exercicio3 from './provas/prova0710/ex3/index.js'
+import Exercicio4 from './provas/prova1110/ex4/index.js'
+import Exercicio5 from './provas/prova1110/ex5/index.js'
+import Exercicio6 from './provas/prova1110/ex6/index.js'
 
 
 
@@ -13,6 +16,9 @@ export const Rotas = () =>{
                 <Route path = '/ex1' element={< Exercicio1/>} />
                 <Route path = '/ex2' element={< Exercicio2/>} />
                 <Route path = '/ex3' element={< Exercicio3/>} />
+                <Route path = '/ex4' element={< Exercicio4/>} />
+                <Route path = '/ex5' element={< Exercicio5/>} />
+                <Route path = '/ex6' element={< Exercicio6/>} />
             </Routes>
         </BrowserRouter>
     );
